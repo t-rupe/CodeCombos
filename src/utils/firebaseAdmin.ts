3387340a5ca84013ef6c2b00ac5a83981652d3fc
t-rupe <personal@trupe.info>
2023-11-20 10:52:45 -0800

    Refactor Firebase Admin initialization

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -3,18 +3,20 @@ import * as admin from 'firebase-admin';
 // Initialize Firebase Admin if it hasn't been initialized yet
 if (!admin.apps.length) {
   try {
-    // Ensure private key has the correct newlines
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+    // Use non-null assertion (!) as we are confident these environment variables are set
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n');
+    const projectId = process.env.FIREBASE_PROJECT_ID!;
+    const clientEmail = process.env.FIREBASE_CLIENT_EMAIL!;
+    const databaseURL = process.env.FIREBASE_DATABASE_URL!;
 
     // Initialize Firebase Admin SDK with the environment variables
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Cast to string to satisfy TypeScript's type checking
-        privateKey: privateKey as string,
+        projectId,
+        clientEmail,
+        privateKey,
       }),
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      databaseURL,
     });
   } catch (error) {
     console.log('Firebase admin initialization error', error);
