92da850dd795e879f7534dc56afc4a0e672caa8a
t-rupe <personal@trupe.info>
2023-11-20 10:50:54 -0800

    Fix newlines in Firebase private key initialization

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -3,15 +3,17 @@ import * as admin from 'firebase-admin';
 // Initialize Firebase Admin if it hasn't been initialized yet
 if (!admin.apps.length) {
   try {
+    // Ensure private key has the correct newlines
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+
+    // Initialize Firebase Admin SDK with the environment variables
     admin.initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY
-          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
-          : undefined,
+        // Cast to string to satisfy TypeScript's type checking
+        privateKey: privateKey as string,
       }),
-    
       databaseURL: process.env.FIREBASE_DATABASE_URL,
     });
   } catch (error) {
